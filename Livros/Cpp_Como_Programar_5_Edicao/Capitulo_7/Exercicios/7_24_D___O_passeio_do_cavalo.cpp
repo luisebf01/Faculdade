44ae838555828eec5ded307f9898c51b9c3b2c00
Luis Eduardo <luisebf01@gmail.com>
2022-08-21 16:40:59 -0300

    Update 7_24_D___O_passeio_do_cavalo.cpp

--- Livros/Cpp_Como_Programar_5_Edicao/Capitulo_7/Exercicios/7_24_D___O_passeio_do_cavalo.cpp
+++ Livros/Cpp_Como_Programar_5_Edicao/Capitulo_7/Exercicios/7_24_D___O_passeio_do_cavalo.cpp
@@ -7,10 +7,10 @@ caminho em forma de L (duas posições em uma direção e então uma em uma dire
 Portanto, a partir de um quadrado no meio de um tabuleiro vazio, o cavalo pode fazer oito 
 movimentos diferentes (numerados de 0 a 7) como mostra a Figura 7.34
 
-d) Escreva uma versão do programa Passeio do Cavalo que, diante de um impasse entre dois ou mais 
-quadrados, decide qual quadrado escolher olhando para a frente aqueles quadrados alcançáveis 
-a partir dos quadrados geradores do impasse. Seu programa deve moverse para o quadrado por meio
-do qual seu próximo movimento chegaria ao quadrado com o número de acessibilidade mais baixo.
+d) Escreva uma versão do programa Passeio do Cavalo que, diante de um impasse entre dois ou mais quadrados,
+decide qual quadrado escolher olhando para a frente aqueles quadrados alcançáveis a partir dos quadrados
+geradores do impasse. Seu programa deve moverse para o quadrado por meio do qual seu próximo movimento 
+chegaria ao quadrado com o número de acessibilidade mais baixo.
 */
 
 #include <iostream>
@@ -21,87 +21,195 @@ using std::endl;
 #include <iomanip>
 using std::setw;
 
+#include <vector>
+using std::vector;
+
 int main()
 {
-    const int sideOfChessBoard = 8;
-    int acessibility[sideOfChessBoard][sideOfChessBoard] = { {2,3,4,4,4,4,3,2},
-                                                             {3,4,6,6,6,6,4,3},
-                                                             {4,6,8,8,8,8,6,4},
-                                                             {4,6,8,8,8,8,6,4},
-                                                             {4,6,8,8,8,8,6,4},
-                                                             {4,6,8,8,8,8,6,4},
-                                                             {3,4,6,6,6,6,4,3},
-                                                             {2,3,4,4,4,4,3,2} };
-
-    int board[sideOfChessBoard][sideOfChessBoard] = { 0 };
-
-    const int emptyBoardSquare = 0; 
-    const int arrayLowerBoundIndex = 0;
-    const int arrayUpperBoundIndex = 7;
+    const int side = 8;
+    int acessibility[side][side] = { {2,3,4,4,4,4,3,2},
+                                     {3,4,6,6,6,6,4,3},
+                                     {4,6,8,8,8,8,6,4},
+                                     {4,6,8,8,8,8,6,4},
+                                     {4,6,8,8,8,8,6,4},
+                                     {4,6,8,8,8,8,6,4},
+                                     {3,4,6,6,6,6,4,3},
+                                     {2,3,4,4,4,4,3,2} };
+                                     
+    int board[side][side] = { {2,3,4,4,4,4,3,2},
+                              {3,4,6,6,6,6,4,3},
+                              {4,6,8,8,8,8,6,4},
+                              {4,6,8,8,8,8,6,4},
+                              {4,6,8,8,8,8,6,4},
+                              {4,6,8,8,8,8,6,4},
+                              {3,4,6,6,6,6,4,3},
+                              {2,3,4,4,4,4,3,2} };
+                              
+    int test[side][side] = { {2,3,4,4,4,4,3,2},
+                             {3,4,6,6,6,6,4,3},
+                             {4,6,8,8,8,8,6,4},
+                             {4,6,8,8,8,8,6,4},
+                             {4,6,8,8,8,8,6,4},
+                             {4,6,8,8,8,8,6,4},
+                             {3,4,6,6,6,6,4,3},
+                             {2,3,4,4,4,4,3,2} };
     
-    const int horizontal[sideOfChessBoard] = { 2, 1, -1, -2, -2, -1, 1, 2 }; // movimentos possiveis em x
-    const int vertical[sideOfChessBoard] = { -1, -2, -2, -1, 1, 2, 2, 1 }; // movimentos possiveis em y
-    const int initialMoveNumber = 0;
-    int moveNumber = 0; // essa variavel combina os 2 arrays acima para movimentar o cavalo
-    int tempMoveNumber = 0;
-    const int maximumMovesNumber = 8; //    
+    const int moveX[side] = { 2, 1, -1, -2, -2, -1, 1, 2 }; // movimentos possiveis em x
+    const int moveY[side] = { -1, -2, -2, -1, 1, 2, 2, 1 }; // movimentos possiveis em y
+    int mN = 0; // moveNumber --- 0 até 7 == 8 movimentos possiveis do cavalo
+    // a variavel acima combina os 2 arrays acima para movimentar o cavalo
     
-    const int smallestAcessibility = 9; // a menor de todas
-    int smallerAcessibility = smallestAcessibility; // a menor do momento
+    int tempMN = 0; // tempMoveNumber
+    int smallAcess = 9;
     
-    int currentColumn = 0; // está para horizontal[sideOfChessBoard]
-    int currentRow = 0; // está para vertical[sideOfChessBoard]
-    int columnTemp = currentColumn; // coluna temporaria para teste
-    int rowTemp = currentRow;  // linha temporaria para teste
+    vector<int> iX;
+    vector<int> iY;
     
-    int numberOfCurrentMovement = 0; // contador do loop de alterações
-    const int numberOfPossibleMovements = 64;
+    vector<int> indiceMN;
+    int sub = 100;
+    int decision = 0;
     
-    int changeCounter = 0; // marcador de alterações máximas
+    int currentRow = 0; // está para moveV[side]
+    int currentColumn = 0; // está para moveH[side]
+    int rTemp = 0;  // linha temporaria para teste
+    int cTemp = 0; // coluna temporaria para teste
     
-    bool alteration = true; // se alterar o tabuleiro = true
+    int currentRow2 = 0; // está para moveV[side]
+    int currentColumn2 = 0; // está para moveH[side]
+    int rTemp2 = 0;  // linha temporaria para teste
+    int cTemp2 = 0; // coluna temporaria para teste
     
-    while(numberOfCurrentMovement < numberOfPossibleMovements && alteration == true) // fica no loop, se houve alterações
-    {        
-        alteration = false;
-        smallerAcessibility = smallestAcessibility;        
-        moveNumber = initialMoveNumber;
-        tempMoveNumber = initialMoveNumber;
-        while(tempMoveNumber < maximumMovesNumber) // verifica os 8 movimentos possiveis, e salva o que tem a menor acessibilidade
+    int i = 0; // contador do loop de alterações
+    int z = 0;
+    
+    bool alteration = false; // se alterar o tabuleiro = true
+    
+    int cR = 0;
+    int cC = 0;
+    
+    while(cR < 8)
+    {
+        cC = 0;
+        while(cC < 8)
         {
-            if((currentRow + vertical[tempMoveNumber]) >= arrayLowerBoundIndex &&
-                (currentRow + vertical[tempMoveNumber]) <= arrayUpperBoundIndex &&
-                (currentColumn + horizontal[tempMoveNumber]) >= arrayLowerBoundIndex &&
-                (currentColumn + horizontal[tempMoveNumber]) <= arrayUpperBoundIndex)
+            int currentColumn = cC; // está para moveH[side]
+            int currentRow = cR; // está para moveV[side]
+            int cTemp = currentColumn; // coluna temporaria para teste
+            int rTemp = currentRow;  // linha temporaria para teste
+            i = 0;
+            while(i < 64) // board
             {
-                rowTemp += vertical[tempMoveNumber];
-                columnTemp += horizontal[tempMoveNumber];
-                if(acessibility[rowTemp][columnTemp] != emptyBoardSquare && 
-                    acessibility[rowTemp][columnTemp] < smallerAcessibility)
+                alteration = false;
+                smallAcess = 9;
+                tempMN = 0;
+                mN = 0;
+                while(mN < 8) // moveNumber
                 {
-                    smallerAcessibility = acessibility[rowTemp][columnTemp];
-                    moveNumber = tempMoveNumber;                    
-                    alteration = true;
+                    if((currentRow + moveY[mN]) >= 0 &&
+                        (currentRow + moveY[mN]) <= 7 &&
+                        (currentColumn + moveX[mN]) >= 0 &&
+                        (currentColumn + moveX[mN]) <= 7)
+                    {
+                        rTemp += moveY[mN];
+                        cTemp += moveX[mN];
+                        if(acessibility[rTemp][cTemp] > 0 && 
+                            acessibility[rTemp][cTemp] < smallAcess)
+                        {
+                            smallAcess = acessibility[rTemp][cTemp]; // menor acessibilidade
+                            tempMN = mN; // indice do movimento do cavalo
+                            alteration = true;
+                            indiceMN.clear();
+                            indiceMN.push_back(mN);
+                        }
+                        else if (acessibility[rTemp][cTemp] == smallAcess)
+                        {
+                            indiceMN.push_back(mN); // salvando os indices do impasse
+                            currentRow2 = rTemp; // salvando a posição do impasse
+                            currentColumn2 = rTemp;
+                            rTemp2 = rTemp;
+                            cTemp2 = cTemp;
+                        }
+                        rTemp = currentRow;
+                        cTemp = currentColumn;
+                    }
+                    mN++;
                 }
-                rowTemp = currentRow;
-                columnTemp = currentColumn;
+                if(indiceMN.size() > 1)
+                {
+                    sub = 100;
+                    decision = 0;
+                    z = 0;
+                    while(z < indiceMN.size())
+                    {
+                        smallAcess = 9;
+                        tempMN = 0;
+                        mN = 0;
+                        while(mN < 8)
+                        {
+                            if((currentRow2 + moveY[mN]) >= 0 &&
+                                (currentRow2 + moveY[mN]) <= 7 &&
+                                (currentColumn2 + moveX[mN]) >= 0 &&
+                                (currentColumn2 + moveX[mN]) <= 7)
+                            {
+                                rTemp2 += moveY[mN];
+                                cTemp2 += moveX[mN];
+                                if(acessibility[rTemp2][cTemp2] > 0 && 
+                                    acessibility[rTemp2][cTemp2] < smallAcess)
+                                {
+                                    smallAcess = acessibility[rTemp2][cTemp2];
+                                }
+                                rTemp2 = currentRow2;
+                                cTemp2 = currentColumn2;
+                            }
+                            mN++;
+                        }
+                        if(smallAcess < sub)
+                        {
+                            sub = smallAcess; // abaixa o nivel
+                            decision = z; // decide pelo indice atual
+                        }
+                        z++;
+                    }
+                    tempMN = indiceMN[decision];
+                }
+                if(alteration == false)
+                    break;
+                currentRow += moveY[tempMN];
+                currentColumn += moveX[tempMN];
+                rTemp = currentRow;
+                cTemp = currentColumn;
+                acessibility[currentRow][currentColumn] = 0;
+                test[currentRow][currentColumn] = i + 1; //dddddd
+                i++;
+            } 
+            if(i == 64)
+            {
+                iX.push_back(cR);
+                iY.push_back(cC);
+                cout << "Sequencia = " << iX.size() 
+                    << "\tl = " << cR << "\tc = " << cC << endl;
+                for(int c = 0; c < 8; c++, cout << endl)
+                    for(int d = 0; d < 8; d++)
+                        cout << setw(4) << test[c][d];
+                cout << endl << endl;
+                for(int m = 0; m < 8; m++)
+                    for(int n = 0; n < 8; n++)
+                        test[m][n] = board[m][n];
             }
-            tempMoveNumber++;
-        }        
-        if (alteration == true)
-        {
-            currentRow += vertical[moveNumber];
-            currentColumn += horizontal[moveNumber];
-            rowTemp = currentRow;
-            columnTemp = currentColumn;
-            acessibility[currentRow][currentColumn] = emptyBoardSquare;
-            board[currentRow][currentColumn] = ++numberOfCurrentMovement;
-            changeCounter = numberOfCurrentMovement;
+            for(int a = 0; a < 8; a++)
+                for(int b = 0; b < 8; b++)
+                    acessibility[a][b] = board[a][b];
+            cC++;
         }
+        cR++;
     }
-    for(int line = 0; line < sideOfChessBoard; line++, cout << endl)
-        for(int column = 0; column < sideOfChessBoard; column++)
-            cout << setw(4) << board[line][column];
-    cout << "\nchangeCounter = " << changeCounter << endl;
+    cout << "\t\tSize = " << iX.size() << endl;
+    cout << "\n      Row =   ";
+    for(int r = 0; r < iX.size(); r++)
+        cout << iX[r] << "   ";
+    cout << endl;
+    cout << "   Column =   ";
+    for(int s = 0; s < iX.size(); s++)
+        cout << iY[s] << "   ";
     return 0;
 }
